d5e5fb2936dc9b02785d16b7bf2a89af5249ef12
Konstantin Tkachuk <konstantin.tkachuk@tu-dortmund.de>
2016-03-18 14:11:05 +0100

    Commit of tutorial code.

---
+++ basics/src/harvester.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Konstantin on 18.03.2016.
+ */
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('harvester'); // -> 'a thing'
+ */
+module.exports = function (creep) {
+
+    if(creep.carry.energy < creep.carryCapacity) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+    }
+    else {
+        if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns.Spawn1);
+        }
+    }
+}
